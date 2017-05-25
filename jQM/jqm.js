22b742557818c6a5ce6d0cc2cc741c93ea97d440
Spam Anytime <Spam Anytime>
2017-05-26 00:35:33 +0700

    mobile

--- jQM/jqm.js
+++ jQM/jqm.js
@@ -1,2 +1,13 @@
 $(document).ready(function(){
 })
+
+function ss() {
+    var $page = $("#page");
+    if ($page.attr("data-theme") === "a") {
+        $page.attr("data-theme", "b");
+    } else {
+        $page.attr("data-theme", "a");
+    }
+
+    $page.page("destroy").page();
+}
