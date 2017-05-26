70b7488c46513f778e3a8b33d3216f5c900be02b
Anh Tuan Pham <anhtuan.pham@kofax.com>
2017-05-26 11:27:25 +0700

    Theme Switch

--- jQM/jqm.js
+++ jQM/jqm.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
 })
 
-function ss() {
+function switchTheme() {
     var $page = $("#page");
+    var clsToRemove;
     if ($page.attr("data-theme") === "a") {
         $page.attr("data-theme", "b");
+        clsToRemove = /\b[\w-]+?-a\b/g;
     } else {
         $page.attr("data-theme", "a");
+        clsToRemove = /\b[\w-]+?-b\b/g;
     }
+
+    $page.each(function(idx, element) {
+        var $element = $(element);
+        var cls = $element.attr("class");
+        cls = cls.replace(clsToRemove, '');
+        $element.attr("class", cls);
+    });
 
     $page.page("destroy").page();
 }
