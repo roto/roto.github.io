e130a8d4cc68dd9ad971009d76072c4a0bc99863
Spam Anytime <Spam Anytime>
2017-06-05 10:49:01 +0700

    Disable jqueyry.finger, and ignore the uninitized page error

--- jQM/jqm.js
+++ jQM/jqm.js
@@ -1,28 +1,6 @@
 $(document).ready(function(){
 })
 
-$(document).on("click", "ul li", function(event) {
-    /*$(this).animate({
-        opacity: 0,
-        left: "+=50%"
-    }, 300, "swing", function () {
-        // Animation complete.
-        $(this).remove();
-    });*/
-}).on("swipeleft", "ul li", function(e) {
-    $(this).animate({
-        left: "-=13%"
-    }, 130, "swing", function () {
-
-    });
-}).on("swiperight", "ul li", function(e) {
-    $(this).animate({
-        left: "+=13%"
-    }, 130, "swing", function () {
-
-    });
-});
-
 function switchTheme() {
     var $pages = $("[data-role='page']");
 
@@ -46,6 +24,16 @@ function switchTheme() {
             }
         });
 
-        $page.page("destroy").page();
+        try {
+            $page.page("destroy");
+        } catch (err) {
+            if (err.message.indexOf("prior to initialization") >= 0) {
+                // page might not be initilized, ignore and continue
+            } else {
+                throw err;
+            }
+        }
+
+        $page.page();
     });
 }
