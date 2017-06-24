175b91ee2a91d9e17a81fe5e17be5c9b93fbebe0
RotO <SpamAnytime@gmail.com>
2017-06-24 14:45:21 +0700

    Hide browser address bar on mobile.

--- jQM/jqm.js
+++ jQM/jqm.js
@@ -33,6 +33,25 @@ $(document).ready(function(){
 	});*/
 })
 
+/* Hide browser address bar on mobile */
+function hideAddressBar() {
+	if (!window.location.hash) {
+		if (document.height < window.outerHeight) {
+			document.body.style.height = (window.outerHeight + 50) + 'px';
+		}
+		setTimeout(function() {
+			window.scrollTo(0, 1);
+		}, 50);
+	}
+}
+
+window.addEventListener("load", function() {
+	if (!window.pageYOffset) {
+		hideAddressBar();
+	}
+});
+window.addEventListener("orientationchange", hideAddressBar);
+
 // temporary initial object
 var initialOrderItems = [{
 		itemID: "pho-bo",
