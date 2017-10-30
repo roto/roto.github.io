6d0cf0347b9cc2d7c80333e00d37f93efdf32b5b
ASIA\AnhTuan.Pham <AnhTuan.Pham@kofax.com>
2017-10-30 12:07:40 +0700

    Vendor: table page, remove popup code.

--- goidat.vn/vendor/page.delivery.js
+++ goidat.vn/vendor/page.delivery.js
@@ -1,37 +1,31 @@
 
 function populateDelivery() {
-	var $div = $('div#delivery #delivery-list');
+	var html = '';
+	for (var i in deliveryData) {
+		var floor = deliveryData[i];
+		var floorHTML = '<fieldset data-role="controlgroup" data-type="horizontal">';
+		floorHTML += '<legend>' + floor.name + '</legend>';
 
-	if ($div.children().length === 0) {
-		$div.hide();
-		var html = '';
-		for (var i in deliveryData) {
-			var floor = deliveryData[i];
-			var floorHTML = '<fieldset data-role="controlgroup" data-type="horizontal">';
-			floorHTML += '<legend>' + floor.name + '</legend>';
-
-			for (var j in floor.seats) {
-				var seat = floor.seats[j];
-				var seatName = 'seat-' + i + '-' + j;
-				if (seat.bills && seat.bills.length > 0) {
-					for (var k in seat.bills) {
-						floorHTML += '<input type="button" id="' + seatName + '"';
-						var billGUID = seat.bills[k];
-						floorHTML += ' style="background-color:' + hash_to_rbg(hash_code(billGUID)) + '"';
-						floorHTML += 'value="' + seat.displayName + '">'
-					}
-				} else {
+		for (var j in floor.seats) {
+			var seat = floor.seats[j];
+			var seatName = 'seat-' + i + '-' + j;
+			if (seat.bills && seat.bills.length > 0) {
+				for (var k in seat.bills) {
 					floorHTML += '<input type="button" id="' + seatName + '"';
+					var billGUID = seat.bills[k];
+					floorHTML += ' style="background-color:' + hash_to_rbg(hash_code(billGUID)) + '"';
 					floorHTML += 'value="' + seat.displayName + '">'
 				}
+			} else {
+				floorHTML += '<input type="button" id="' + seatName + '"';
+				floorHTML += 'value="' + seat.displayName + '">'
 			}
-
-			floorHTML += '</fieldset>';
-
-			html = floorHTML + html; // revert the floor order
 		}
 
-		$div.html(html).enhanceWithin();
-		$div.show();
-	}		
+		floorHTML += '</fieldset>';
+
+		html = floorHTML + html; // revert the floor order
+	}
+
+	$('div#delivery #delivery-list').html(html).enhanceWithin();
 }
