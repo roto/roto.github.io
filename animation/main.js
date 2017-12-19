08dfa9d6b552fb6487146c4df34b5cf21a77ae44
ASIA\AnhTuan.Pham <AnhTuan.Pham@kofax.com>
2017-12-19 16:03:05 +0700

    SVG animation sample.

---
+++ animation/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	$.get('sample.svg')
+			.then(injectSvg)
+			.always(startAnimation);
+});
+
+function injectSvg(xmlDoc) {
+	var svg = $(xmlDoc);//.find('svg');
+	var container = $("#container");
+	container.append(svg);
+}
+
+function startAnimation() {
+	var paths = $('.squiggle-animated path');
+	paths.each(function(index, path) {
+		initPathDrawing(path);
+	});
+}
+
+var speed = 60.0;
+var delay = 0.0;
+
+function initPathDrawing(path) {
+	// var path = document.querySelector('.squiggle-animated path');
+	var length = path.getTotalLength();
+	var duration = Math.sqrt(length) / speed;
+
+	// Clear any previous transition
+	path.style.transition = path.style.WebkitTransition = 'none';
+	// Set up the starting positions
+	path.style.strokeDasharray = length + ' ' + length;
+	path.style.strokeDashoffset = length;
+
+	// Trigger a layout so styles are calculated & the browser
+	// picks up the starting position before animating
+	path.getBoundingClientRect();
+	// Define our transition
+	path.style.transitionProperty = path.style.WebkitTransitionProperty = 'stroke-dashoffset';
+	path.style.transitionTimingFunction = path.style.WebkitTransitionTimingFunction = 'ease-in-out';	
+	path.style.transitionDuration = path.style.WebkitTransitionDuration = duration + 's';
+	path.style.transitionDelay = path.style.WebkitTransitionDelay = delay + 's';
+	// Go!
+	path.style.strokeDashoffset = '0';
+
+	delay += duration;
+}
