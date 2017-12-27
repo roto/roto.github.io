0a2a4daca2817daf1739c236e8e72fef790ad38d
ASIA\AnhTuan.Pham <AnhTuan.Pham@kofax.com>
2017-12-27 13:42:30 +0700

    Merge the separated path chain.

--- animation/main.js
+++ animation/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$.get('BB_00007.svg')
 			.then(injectSvg)
+			.always(mergePath)
 			.always(startAnimation);
 });
 
@@ -11,6 +12,89 @@ function injectSvg(xmlDoc) {
 		svg = $(xmlDoc);
 	}
 	$("#container").append(svg);
+}
+
+var pathSegmentPattern = /[a-z][^a-z]*/ig;
+var pathPointPattern = /([a-z]+)([-+]?[0-9]*\.?[0-9]*),([-+]?[0-9]*\.?[0-9]*)/ig;
+
+function mergePath() {
+	var paths = $('.squiggle-animated path');
+
+	var lastPath;
+
+	paths.each(function(index, path) {
+		var stroke = path.getAttribute('stroke');
+		if (stroke == "none") {
+			path.remove();
+			return;
+		}
+
+		var data = path.getAttribute('d');
+
+		var segments = data.match(pathSegmentPattern);
+
+		if (!lastPath) {
+			lastPath = path;
+		} else {
+			var firstSeg = segments[0];
+			pathPointPattern.lastIndex = 0;
+			var point = pathPointPattern.exec(firstSeg);
+
+			if (point[1] == "M") {
+				var $lastPath = $(lastPath);
+				var x = $lastPath.data('x');
+				var y = $lastPath.data('y');
+
+				if (Math.abs(x - parseFloat(point[2])) < 0.1 &&
+						Math.abs(y - parseFloat(point[3])) < 0.1) {
+					// connected path
+					path.remove();
+					var lastData = lastPath.getAttribute('d');
+					for (var i = 1; i < segments.length; ++i) {
+						lastData += segments[i];
+					}
+					lastPath.setAttribute('d', lastData);
+				} else {
+					// wrap the last path up
+					$lastPath.removeData('x');
+					$lastPath.removeData('y');
+					// init the next path chain
+					lastPath = path;
+				}
+			} else {
+				throw "First point is not M command";
+			}
+		}
+
+		updatePathData(lastPath, segments);
+	});
+}
+
+function updatePathData(path, segments) {
+	var x = 0.0;
+	var y = 0.0;
+
+	for (var i = segments.length - 1; i >= 0; --i) {
+		var seg = segments[i];
+		pathPointPattern.lastIndex = 0;
+		var point = pathPointPattern.exec(seg);
+
+		if (!point) {
+			var a = 1;
+		}
+
+		if (point[1] == "m" || point[1] == "l") {
+			x += parseFloat(point[2]);
+			y += parseFloat(point[3]);
+		} else if (point[1] == "M" || point[1] == "L") {
+			var $path = $(path);
+			$path.data('x', parseFloat(point[2]) + x);
+			$path.data('y', parseFloat(point[3]) + y);
+			return;
+		} else {
+			throw "Unrecognized path command: " + point[1];
+		}
+	}
 }
 
 function startAnimation() {
@@ -34,7 +118,7 @@ function sortByClass(paths) {
 	});
 }
 
-var speed = 600.0;
+var speed = 60.0;
 var delay = 0.0;
 
 function initPathDrawing(path) {
